5037d2f9931c873f5bdc13dcf185aa15ed01e212
Dan Turkenkopf <dturkenk@gmail.com>
2013-08-12 20:59:37 -0400

    Checking in missed items

---
+++ Dapper.ColumnMapper/ColumnTypeMapper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Dapper.ColumnMapper
+{
+    public class ColumnTypeMapper : SqlMapper.ITypeMap
+    {
+        private SqlMapper.ITypeMap _internalMapper;
+
+        public ColumnTypeMapper(Type type)
+        {
+            _internalMapper = new CustomPropertyTypeMap(type, _propertyResolver);
+        }
+
+        public ConstructorInfo FindConstructor(string[] names, Type[] types)
+        {
+            return _internalMapper.FindConstructor(names, types);
+        }
+
+        public SqlMapper.IMemberMap GetConstructorParameter(ConstructorInfo constructor, string columnName)
+        {
+            return _internalMapper.GetConstructorParameter(constructor, columnName);
+        }
+
+        public SqlMapper.IMemberMap GetMember(string columnName)
+        {
+            return _internalMapper.GetMember(columnName);
+        }
+
+        private readonly Func<Type, string, PropertyInfo> _propertyResolver = (type, name) =>
+        {
+            var properties =
+                type.GetProperties(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic |
+                                   BindingFlags.Public);
+
+            return properties.FirstOrDefault(property =>
+            {
+                if (property.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+
+                var attribute = property.GetCustomAttribute<ColumnMappingAttribute>();
+
+                return attribute != null &&
+                       attribute.ColumnName.Equals(name, StringComparison.OrdinalIgnoreCase);
+            });
+        };
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColumnMappingAttribute : Attribute
+    {
+        public string ColumnName { get; set; }
+
+        public ColumnMappingAttribute(string columnName)
+        {
+            ColumnName = columnName;
+        }
+    }
+}
